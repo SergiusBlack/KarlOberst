ebc613356f72b7db2789ea365fa19256b6922f24
Sergio Oberst <pogonero91@gmail.com>
2016-06-05 01:45:04 +0200

    Subida

---
+++ KarlOberstV2/Controllers/StoreController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KarlOberstV2.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public string Index()
+        {
+            //return View();
+            return "Método indice de el controlador tienda";
+        }
+
+        public string Buscar(string genero)
+        {
+            //return View();
+            string msg = HttpUtility.HtmlEncode("Tienda.Buscar, Genero = " + genero);
+            return msg;
+        }
+
+        public string Detalles(int id)
+        {
+            //return View();
+            return "Tienda.Detalles, ID = " + id;
+        }
+    }
+}
---
+++ KarlOberstV2/Models/Genero.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KarlOberstV2.Models
+{
+    public class Genero
+    {
+        public string Nombre { get; set; }
+    }
+}
